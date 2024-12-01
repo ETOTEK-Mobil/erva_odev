54378f44f32fa96a9ae7f35a03237426767262b8
Erva Şevval Şimşek <ervasevvalsmk@gmail.com>
2024-12-01 17:29:20 +0300

    İmage Page düzeltme yapıldı

--- lib/ui/view/hafta_2/images_page.dart
+++ lib/ui/view/hafta_2/images_page.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:odev/gen/assets.gen.dart';
 
 class ImagesPage extends StatefulWidget {
   const ImagesPage({super.key});
@@ -13,15 +14,16 @@ class _ImagesPageState extends State<ImagesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Images"),
+          title: const Text("Images"),
         ),
         body: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Image.asset(Assets.images.croppedEtotekPhotoroom.path),
             CachedNetworkImage(
               imageUrl:
                   'https://etotek.com.tr/wp-content/uploads/2024/10/cropped-etotek-Photoroom.png',
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Icon(Icons.error),
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
           ]),
         ));
